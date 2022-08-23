c60cdb910e93f17f1026d4169f02f02ff6725b51
Ariel Valdez <arielvaldez017@outlook.com>
2022-08-23 18:58:10 -0400

    Components created front-end

--- Front-End/Service/components/completionBar.js
+++ Front-End/Service/components/completionBar.js
@@ -1,1 +1,23 @@
-// PlaceHolder component
+// PlaceHolder componentz
+import React from 'react';
+import PropType from 'prop-types';
+
+const completionBar = ( { value, max } ) => {
+    return (
+        <progress value = {value} max = {max} />
+    )
+};
+
+completionBar.propTypes {
+    value: PropTypes.number.isRequired,
+    max: PropTypes.number,
+}
+
+completionBar.defaultProps {
+    value: 0,
+    max: 100,
+}
+
+export default completionBar;
+
+// Video: https://www.youtube.com/watch?v=3sH_Kq9e5hQ
