fe4d6c9d2fe0868fd3cea71576cd2045656f2e08
Ariel Valdez <arielvaldez017@outlook.com>
2022-08-28 21:13:25 -0400

    login pass

--- Front-End/Service/components/login/login.js
+++ Front-End/Service/components/login/login.js
@@ -3,14 +3,43 @@
 import React from 'react';
 import "./login.css";
 
+const [popupStyle, showPopup] = useState("hide")
+
+const popup = () => {
+    showPopup("login-popup")
+    setTimeout(() => showPopup("hide"), 3000)
+}
+
 const LoginForm = () => {
     return (
-        <div>
+        <div className="cover">
+            <h1> Login </h1>
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
             
+            <div className="login-btn" onClick={popup}> Login </div>
+
+            {/* This block below offers an alternate login for the user,
+            that may or may not be required for this app */}
+
+            <p className="text"> Or login using </p>
+            <div className="alt-login">
+                <div className="facebook"> </div>
+                <div className="google"> </div>
+            </div>
+
+            <div className={"popupStyle"}>
+                <h3> Login Failed </h3>
+                <p> Username or Password Incorrect</p>
+            </div>
+
         </div>
     )
 }
 
 export default login;
 
-//Video: https://www.youtube.com/watch?v=d1j1NcRMvV0
+//Video: https://www.youtube.com/watch?v=4BhhGs0PFHU
+//Another Video: https://www.youtube.com/watch?v=W1RWa0aU1pE&t=2s
+//Yet Another Video: https://www.youtube.com/watch?v=d1j1NcRMvV0 | https://github.com/moses-netshitangani/responsive-login-form
+//Using hooks: https://www.youtube.com/watch?v=8hU0I8rY4u4
