f2b7f849cbdcbe2fd72d3d03b665b677e3e1ce83
Ariel Valdez <arielvaldez017@outlook.com>
2022-10-02 07:57:27 -0400

    Added enum: Conditions

---
+++ Back-End/components/enums/Conditiones.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace enums
+{
+    // Revise later
+    public enum Condition{
+        Reparado,
+        Pintado,
+        Desabolladura,
+        Terminado,
+        Cotizacion
+    }
+}
