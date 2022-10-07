11e77a754637106f41f1fd0f9cbef91715f642c1
Ariel Valdez <arielvaldez017@outlook.com>
2022-10-07 00:31:31 -0400

    Added empty controllers

---
+++ MWST-Backend/MWST-API/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MWST_API.Controllers
+{
+    [Route("api/Service")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+    }
+}
