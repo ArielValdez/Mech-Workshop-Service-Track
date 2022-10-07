f1973ad60a9860f7cbb42887b33923bbef8c0262
Ariel Valdez <arielvaldez017@outlook.com>
2022-10-07 09:25:46 -0400

    added classes and controllers for location.

---
+++ MWST-Backend/MWST-API/Controllers/ProvinciaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MWST_API.Controllers
+{
+    [Route("api/Provincia")]
+    [ApiController]
+    public class ProvinciaController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Connection con = new Connection();
+        private UserModel models = new UserModel();
+
+        public ProvinciaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            // This should not delete a row. Instead, put a user as "ïnactive".
+            return new JsonResult("Not implemented yet.");
+        }
+
+        [HttpPost]
+        public JsonResult Post()
+        {
+            // This should not delete a row. Instead, put a user as "ïnactive".
+            return new JsonResult("Not implemented yet.");
+        }
+
+        [HttpPut]
+        public JsonResult Put()
+        {
+            // This should not delete a row. Instead, put a user as "ïnactive".
+            return new JsonResult("Not implemented yet.");
+        }
+
+        [HttpDelete]
+        public JsonResult Delete()
+        {
+            // This should not delete a row. Instead, put a user as "ïnactive".
+            return new JsonResult("Not implemented yet.");
+        }
+    }
+}
