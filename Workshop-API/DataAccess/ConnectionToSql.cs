91393f32cb0037fb6a0f77dee7719fb46062dc5b
Ariel Valdez <arielvaldez017@outlook.com>
2022-10-14 09:32:18 -0400

    fixed connection strings

--- Workshop-API/DataAccess/ConnectionToSql.cs
+++ Workshop-API/DataAccess/ConnectionToSql.cs
@@ -9,7 +9,7 @@ namespace DataAccess
         public ConnectionToSql()
         {
             // Connection String to an Azure Database
-            connectionString = "Server=proyectofinal-server.database.windows.net;Database=Db administrador;integrated security=true";
+            connectionString = "Server=proyectofinal-server.database.windows.net;Database=Db administrador;User Id=Administrador;password=Admin12345@.;integrated security=true";
         }
 
         protected SqlConnection GetConnection()
