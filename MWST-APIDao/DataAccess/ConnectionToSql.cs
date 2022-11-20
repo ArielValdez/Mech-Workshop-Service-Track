4611a215ff3df70ab04c00ae892a442dc36a6a46
Ariel Valdez <arielvaldez017@outlook.com>
2022-11-20 15:09:44 -0400

    updated connection strings

--- MWST-APIDao/DataAccess/ConnectionToSql.cs
+++ MWST-APIDao/DataAccess/ConnectionToSql.cs
@@ -9,7 +9,7 @@ namespace DataAccess
         public ConnectionToSql()
         {
             // Connection String to an Azure Database
-            connectionString = "Server=proyectofinal-server.database.windows.net;Database=Db administrador;User Id=Administrador;password=Admin12345@.;integrated security=true";
+            connectionString = "Server=db-mwst.chi0aivlb8yi.us-east-2.rds.amazonaws.com;Database=rdsadmin;User Id=admin;password=Grupo4IDS333;integrated security=true";
         }
 
         protected SqlConnection GetConnection()
